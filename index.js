356af11d5d619545477b75208f882617257e23cd
Michael Ramos <ramosmichael@live.com>
2022-03-01 06:07:34 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Add your code here
+/*const formData = {
+  dogName: 'Byron',
+  dogBreed: 'Poodle',
+};
+
+const configurationObject = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch('http://localhost:3000/dogs', configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error){
+    alert('Bad things! Ragnarők!');
+    console.log(error.message);
+});*/
+
+
+
+function addContact(id) {
+ document.querySelector('body').innerHTML = id;
+ }
+
+function submitData(name, email){
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+  .then(response => response.json())
+  .then(body => addContact(body.id))
+  .catch(function (error) {
+    alert(error)
+    document.body.append(error);
+  })
+}
+
